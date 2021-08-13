361a9e14986d6481b0ae808f1467f158a7e3ee9a
Phạm  Quyết Thắng <quyetthang.phucchi@gmail.com>
2021-08-13 09:57:55 +0700

    model building done

--- src/models/doc/interface.ts
+++ src/models/doc/interface.ts
@@ -1,7 +1,35 @@
+export interface IRequest {
+  field: string;
+  type: string;
+  is_require: boolean;
+  root: boolean;
+  child: string;
+}
+
+interface IStatus {
+  name: string;
+  code: number;
+  description: string;
+}
+
+export enum METHOD_API {
+  GET = 'GET',
+  HEAD = 'HEAD',
+  POST = 'POST',
+  PUT = 'PUT',
+  PATCH = 'PATCH',
+  DELETE = 'DELETE',
+  CONNECT = 'CONNECT',
+  OPTIONS = 'OPTIONS',
+  TRACE = 'TRACE',
+}
 export default interface IDoc {
   _id: string;
-  method: string;
+  method: METHOD_API;
   endpoint: string;
+  request: IRequest[];
+  response: object[];
+  status: IStatus[];
   description: string;
   members: string[];
   extension: string[];
--- src/models/doc/model.ts
+++ src/models/doc/model.ts
@@ -2,6 +2,26 @@ import mongoose from 'mongoose';
 
 const { Schema } = mongoose;
 
+const docRequestSchema = new Schema({
+  field: {
+    type: String,
+  },
+  type: {
+    type: String,
+  },
+  is_require: { type: Boolean },
+  root: {
+    type: Boolean,
+    default: false,
+  },
+  child: {
+    type: Schema.Types.ObjectId,
+    ref: 'DocRequests',
+  },
+});
+
+export const docRequests = mongoose.model('DocRequests', docRequestSchema);
+
 const docSchema = new Schema({
   method: {
     type: String,
@@ -18,5 +38,48 @@ const docSchema = new Schema({
       'TRACE',
     ],
   },
-  //   endpoint
+  endpoint: {
+    type: String,
+    require: true,
+  },
+  request: [docRequestSchema],
+  response: [
+    {
+      type: Object,
+    },
+  ],
+  status: [
+    {
+      name: {
+        type: String,
+      },
+      code: {
+        type: Number,
+      },
+      description: {
+        type: String,
+      },
+    },
+  ],
+  description: {
+    type: String,
+    require: true,
+  },
+  members: [
+    {
+      id_member: {
+        type: Schema.Types.ObjectId,
+        ref: 'Accounts',
+      },
+      name: {
+        type: String,
+        require: true,
+      },
+    },
+  ],
+  extension: {
+    type: String,
+  },
 });
+
+export default mongoose.model('Docs', docSchema);
