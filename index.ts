0bc3e588110935cb3406e94b6155c0fd6686d7c1
Phạm  Quyết Thắng <quyetthang.phucchi@gmail.com>
2021-11-09 23:01:01 +0700

    them socket

--- index.ts
+++ index.ts
@@ -1,12 +1,15 @@
 import 'dotenv/config';
 import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import rootRouter from './src/apis/router';
 import Logger from './src/common/helpers/Logger';
 import errorMiddleware from './src/common/middleware/errorHandlers';
-// import { expressValidator } from './src/apis/user/validator/register';
+
 const app = express();
+
 const PORT = process.env.PORT || 5000;
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '30mb', extended: true }));
@@ -38,6 +41,27 @@ if (!connectString) {
     .catch((err) => Logger.error(err));
 }
 
-app.listen(PORT, function () {
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+  /* options */
+  cors: {
+    origin: '*',
+  },
+});
+app.set('socketIo', io);
+io.on('connection', (socket) => {
+  console.info('connect socket', socket.id);
+  socket.on('connect_room', (room) => {
+    console.log("vao room", room)
+    socket.join(room)
+  })
+  socket.on('disconnect', function () {
+    Logger.warn('user ' + socket.id + ' disconnected');
+    Logger.warn('socket disconnected : ' + socket.id);
+  });
+});
+
+httpServer.listen(PORT, function () {
   console.info(`server is running on port: ${PORT}`);
 });
